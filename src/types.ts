7118afc0fc4bd2b6625118b9f3ccb91bf3376421
Mazen Othman <112982340+MazenOth@users.noreply.github.com>
2024-09-07 13:50:43 +0300

    feat(types): create User interface

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface User {
+  user_id?: number;
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  City: { name: string };
+  roleName: string;
+  email: string;
+}

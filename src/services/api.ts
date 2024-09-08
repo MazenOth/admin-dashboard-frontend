d48682df57225b125ac475f72011747ca2cbfaf4
Mazen Othman <112982340+MazenOth@users.noreply.github.com>
2024-09-08 12:04:53 +0300

    feat(api): create Matchings endpoints

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,9 +5,34 @@ const api = axios.create({
   baseURL: 'http://localhost:5000/api',
 });
 
+// Users Endpoints
 export const getUsers = (roleName: { roleName: string }) =>
   api.get('/users', { params: roleName });
 export const createUser = (user: User) => api.post('/users', user);
 export const updateUser = (id: number, user: User) =>
   api.put(`/users/${id}`, user);
 export const deleteUser = (id: number) => api.delete(`/users/${id}`);
+
+// Matchings Endpoints
+export const getUnmatchedClients = (paginationOptions: {
+  size: number;
+  page: number;
+}) => api.get('/matchings/unmatched/clients', { params: paginationOptions });
+export const getPotentialMatches = (
+  clientId: number,
+  paginationOptions: {
+    size: number;
+    page: number;
+  }
+) =>
+  api.get(`/matchings/potential/${clientId}`, {
+    params: paginationOptions,
+  });
+export const getMatchedUsers = (paginationOptions: {
+  size: number;
+  page: number;
+}) => api.get(`/matchings/users`, { params: paginationOptions });
+export const assignHelper = (id: { clientId: number; helperId: number }) =>
+  api.post(`/matchings/assign`, id);
+export const unassignHelper = (id: { clientId: number; helperId: number }) =>
+  api.post(`/matchings/unassign`, id);

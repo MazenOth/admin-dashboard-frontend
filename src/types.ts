473eeebac7e5f766b7a60ddfc800ba711838dbec
Mazen Othman <112982340+MazenOth@users.noreply.github.com>
2024-09-08 12:05:42 +0300

    wip(types): add new User properties

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,13 @@
 export interface User {
   user_id?: number;
+  client_id: number;
+  helper_id: number;
   first_name: string;
   last_name: string;
   phone_number: string;
   City: { name: string };
+  city_name: string;
+  city_id: number;
   roleName: string;
   email: string;
 }

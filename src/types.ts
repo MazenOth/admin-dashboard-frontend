c5491884cb4d50795eecf6a5ac6979e183c53efe
Mazen Othman <112982340+MazenOth@users.noreply.github.com>
2024-09-09 22:17:27 +0300

    feat(types): create MatchedUser interface

--- src/types.ts
+++ src/types.ts
@@ -5,9 +5,23 @@ export interface User {
   first_name: string;
   last_name: string;
   phone_number: string;
-  City: { name: string };
   city_name: string;
   city_id: number;
-  roleName: string;
+  role_name: string;
   email: string;
+}
+
+export interface MatchedUser {
+  matching_id: number;
+  helper_id: number;
+  client_id: number;
+  client_first_name: string;
+  client_last_name: string;
+  client_phone_number: string;
+  client_email: string;
+  helper_first_name: string;
+  helper_last_name: string;
+  helper_phone_number: string;
+  helper_email: string;
+  city_name: string;
 }
